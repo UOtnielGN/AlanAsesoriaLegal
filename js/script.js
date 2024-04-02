dbcf052f22d810968ab7f72e56cff59f0e66bc1e
UOtnielGN <165592879+UOtnielGN@users.noreply.github.com>
2024-04-01 22:21:32 -0600

    add inde

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // script.js
+
 document.addEventListener("DOMContentLoaded", function () {
     const titleContainer = document.getElementById('titleContainer');
     const title = document.getElementById('title');
@@ -9,20 +10,19 @@ document.addEventListener("DOMContentLoaded", function () {
             titleContainer.classList.remove('clicked');
         }, 1000); // 1000 milisegundos = 1 segundo
     });
-    
-document.addEventListener("DOMContentLoaded", function () {
+
     // Animación de desplazamiento suave para los enlaces internos
     const links = document.querySelectorAll('a[href^="#"]');
-    
+
     for (const link of links) {
         link.addEventListener("click", clickHandler);
     }
-    
+
     function clickHandler(e) {
         e.preventDefault();
         const href = this.getAttribute("href");
         const offsetTop = document.querySelector(href).offsetTop;
-    
+
         scroll({
             top: offsetTop,
             behavior: "smooth"
@@ -40,6 +40,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
         element.addEventListener('mouseleave', () => {
             element.style.backgroundColor = 'transparent'; // Revertir al color original
+        });
+    });
+
+    const images = document.querySelectorAll('.blog-image');
+
+    images.forEach(image => {
+        image.addEventListener('click', function () {
+            const linkId = image.dataset.link;
+            const link = document.getElementById(linkId);
+
+            if (link) {
+                link.click(); // Simula el clic en el enlace "Leer más"
+            }
         });
     });
 });
